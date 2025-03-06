0c4f3df79414f1ba1b2f47e732317f0ed6adae75
jishnupv709 <jpv8086@gmail.com>
2025-03-07 03:14:05 +0530

    Access Control  - allow PUT method

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200','https://inventory-app-orcin-delta.vercel.app/');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   next();
 });
 
